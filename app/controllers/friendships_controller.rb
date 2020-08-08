bd3c53500c6cacbc2152111c4ce227856d8810fc
Adewale Orotayo <didymus7007@gmail.com>
2020-08-08 23:53:15 +0100

    Modified friendship to receive two rows for friendship requests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,9 @@ class FriendshipsController < ApplicationController
     return if current_user.id == params[:user_id]
     return unless current_user.viable_friend?(User.find(params[:user_id]))
 
-    @friendship = current_user.send_a_request(params[:user_id])
+    # @friendship = current_user.send_a_request(params[:user_id])
+    
+    @friendship = Friendship.request(current_user.id, params[:user_id] )
 
     if @friendship.save
       flash[:notice] = 'Friend request sent!'
@@ -14,9 +16,9 @@ class FriendshipsController < ApplicationController
   end
 
   def accept_request
-    @friendship = Friendship.find_by_user_id_and_friend_id(params[:user_id], current_user.id)
+    @friendship = Friendship.accept(current_user.id, params[:user_id])
 
-    @friendship.status = 'accepted'
+    # @friendship.status = 'accepted'
     if @friendship.save
       flash[:notice] = 'Friend Request Accepted!'
     else
@@ -26,10 +28,10 @@ class FriendshipsController < ApplicationController
   end
 
   def decline_request
-    @friendship = Friendship.find_by_user_id_and_friend_id(params[:user_id], current_user.id)
+    Friendship.decline(params[:user_id], current_user.id)
 
-    @friendship.status = 'declined'
-    @friendship.destroy
+    # @friendship.status = 'declined'
+    # @friendship.destroy
     flash[:notice] = 'Friend Request Declined!'
     redirect_back(fallback_location: root_path)
   end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,9 +3,44 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
   validates_presence_of :user_id, :friend_id, uniqueness: true
-  validates_presence_of :status, acceptance: { accept: %w[requested accepted declined] }
+  validates_presence_of :status, acceptance: { accept: %w[pending, requested accepted declined] }
 
-  scope :with_status, ->(status) { where 'status = ?', status }
   scope :friends, -> { where(status: 'accepted') }
-  scope :not_friends, -> { where(status: 'requested') }
+  scope :not_friends, -> { where(status: ['requested', 'pending']) }
+
+  def self.exists?(user, friend)
+    not find_by_user_id_and_friend_id(user, friend).nil?
+  end
+
+  def self.request(sender, friend)
+    unless sender == friend || Friendship.exists?(sender, friend)
+      transaction do
+        create(user_id: sender, friend_id: friend, status: 'pending')
+        create(user_id: friend, friend_id: sender, status: 'requested')
+      end
+    end
+  end
+
+  def self.accept(user, friend)
+    transaction do
+      updated_at = Time.now
+      accept_one_part(user, friend, updated_at)
+      accept_one_part(friend, user, updated_at)
+    end
+  end
+
+  def self.decline(user, friend)
+    transaction do
+      destroy(find_by_user_id_and_friend_id(user, friend))
+      destroy(find_by_user_id_and_friend_id(friend, user))
+    end
+  end
+
+  def self.accept_one_part(user, friend, updated_at)
+    request = find_by_user_id_and_friend_id(user, friend)
+    request.status = 'accepted'
+    request.updated_at = updated_at
+    # request.save
+  end
+
 end
